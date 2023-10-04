290edbe2c89b540e9b1189155cc694e702c19463
Aardsnark <Aardsnark>
2023-10-04 12:08:43 +0200

    make change

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 @app.route('/<random_string>')
 def return_backwards_string(random_string):
     #raise Exception()
+    #testSecret branch test mod
     return "".join(reversed(random_string))
 
 @app.route('/get-mode')
